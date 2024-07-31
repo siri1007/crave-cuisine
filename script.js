2273595bad70fb77eae8a8a57431d952cbe2406c
siri1020 <siridundigala@gmail.com>
2024-07-31 18:12:21 +0530

    Modified

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-document.querySelector('.bar').addEventListener('click', function() {
-    const navbar = document.getElementById('navbar');
-    navbar.classList.toggle('show');
-});
+
+
+
+
+
+// script.js
+function toggleNav() {
+    const nav = document.getElementById("navbar");
+    const heading = document.querySelector(".heading");
+    const content = document.querySelector(".content");
+  
+
+    // Toggle the 'show' class
+    nav.classList.toggle("show");
+
+    // Check if the navbar is visible and adjust the margin and height
+    if (nav.classList.contains("show")) {
+        heading.style.marginTop = "300px"; 
+       content.style.height = "700px"; 
+
+       
+        
+    } else {
+        heading.style.marginTop = "100px";
+
+        content.style.height = "500px"; 
+       heading.style.transition = "0.4s linear";
+    }
+      
+       
+    
+}
+
+
+
+
+
+
